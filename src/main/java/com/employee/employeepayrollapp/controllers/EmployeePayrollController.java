b142e71c74aedfa0e885e989767bb3919119b45b
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-12-17 03:32:29 +0530

    [Kirti]Added:validation to dto and global exception class for all exception thrown by the controller

--- src/main/java/com/employee/employeepayrollapp/controllers/EmployeePayrollController.java
+++ src/main/java/com/employee/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -2,6 +2,8 @@ package com.employee.employeepayrollapp.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -42,15 +44,15 @@ public class EmployeePayrollController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+	public ResponseEntity<ResponseDTO> addEmployeePayrollData(@Valid@RequestBody EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData=null;
 		empData =employeePayrollService.createEmployeePayrollData(empPayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Created EmployeePayroll Data Success", empData);
 		return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 	}
 
-	@PutMapping("/update")
-	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,@RequestBody EmployeePayrollDTO empPayrollDTO) {
+	@PutMapping("/update/{empId}")
+	public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@Valid@PathVariable("empId") int empId,@Valid@RequestBody EmployeePayrollDTO empPayrollDTO) {
 		EmployeePayrollData empData=null;
 		empData =employeePayrollService.updateEmployeePayrollData(empId,empPayrollDTO);
 		ResponseDTO respDTO = new ResponseDTO("Updated EmployeePayroll Data Success  ", empData);
