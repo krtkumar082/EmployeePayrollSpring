d1a1908a834854e8d748f96fdaae00d16c896048
kkumar2_be16@thapar.edu <kkumar2_be16@thapar.edu>
2020-12-18 01:50:03 +0530

    [Kirti]Added:custom query in repository,called from controller to service to repository to fetch department wise employees from database

--- src/main/java/com/employee/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/employee/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -1,9 +1,13 @@
 package com.employee.employeepayrollapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.employee.employeepayrollapp.model.EmployeePayrollData;
 
 public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData,Integer>{
-
+     @Query(value="select * from employee_payroll,employee_department where employee_id=id and department =:department",nativeQuery=true)
+      List<EmployeePayrollData> findEmployeesByDepatment(String department);
 }
